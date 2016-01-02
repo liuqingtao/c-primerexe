8bfcf113ba69815a114f5830062edf0046a481c1
liuqingtao <liuqingtao86@gmail.com>
2016-01-02 23:29:52 +0800

    2.2

---
+++ InitTest/InitTest.cpp
@@ -0,0 +1,17 @@
+// InitTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include<string>
+using namespace std;
+int a;
+std::string b;
+int main()
+{
+	//int a;
+	std::cout << a << std::endl;
+	cout << b;
+    return 0;
+}
+
